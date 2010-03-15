509b3a1a70d382f8ea92042c2edb0276cabce981
Icetigris <Icetigris@9fadbaa6-2c71-11df-82fb-b9de231f855c>
2010-03-15 15:39:30 +0000

    Added Kevin's code changes, but commented out references to icons I don't have.

--- trunk/WorldWeaverGame/Game Source/Planet.cs
+++ trunk/WorldWeaverGame/Game Source/Planet.cs
@@ -12,13 +12,25 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace WorldWeaver
 {
-    class Planet : CelestialBody
+    class Planet : CelestialBody, IDrawableObject, ILoadableObject, IUpdatableObject
     {
         //has an atmosphere
         //core composition (does it have a magnetosphere?)
+
+
+        #region Declarations
+
+        private ContentManager content;
+        private int mySceneIndex;
+        private Model model;
+        private Matrix world;
+        private ChaseCamera camera;
+
+        #endregion
 
         #region Constructor
 
@@ -29,6 +41,10 @@ namespace WorldWeaver
             this.Position = position;
             this.R = r;
             this.mPool = pool;
+
+            // More constructor data, prevents null variables
+            this.content = Globals.contentManager;
+            this.camera = Globals.ChaseCamera;
         }
 
         #endregion
@@ -38,9 +54,12 @@ namespace WorldWeaver
         //if mass of the planet is at least 30% due to silver particles, you get a magnetic field
         public void hasMagneticField()
         {
+            // Get total quantity of particles so we can do the math
+            //int numParticles = mPool.Particles;
+
             foreach (Particle p in mPool.Particles)
             {
-                //
+
             }
         }
 
@@ -60,5 +79,58 @@ namespace WorldWeaver
         }
 
         #endregion
+
+        #region Draw Methods
+
+        public void Update()
+        {
+        }
+
+        //When the scene graph calls each loadable object's LoadContent(), 
+        //they each Content.Load their own models
+        public void LoadContent()
+        {
+            model = content.Load<Model>("Models\\donutplanet");
+            ReadyToRender = true;
+        }
+
+        public void UnloadContent()
+        {
+            //crap
+        }
+        //bool doOnce = true;
+        public new void Draw(GameTime gameTime)
+        {
+            if (!Globals.gameplayScreenDestroyed)
+            {
+                //Particle's world matrix
+                world = Matrix.CreateTranslation(Position);
+
+                Matrix[] transforms = new Matrix[model.Bones.Count];
+                model.CopyAbsoluteBoneTransformsTo(transforms);
+
+                foreach (ModelMesh mesh in model.Meshes)
+                {
+                    foreach (BasicEffect effect in mesh.Effects)
+                    {
+                        effect.EnableDefaultLighting();
+                        effect.PreferPerPixelLighting = true;
+
+                        effect.World = transforms[mesh.ParentBone.Index] * world;
+
+                        // Use the matrices provided by the chase camera
+                        effect.View = camera.View;
+                        effect.Projection = camera.Projection;
+                    }
+
+                    mesh.Draw();
+                }
+            }
+        }
+
+        #endregion
+
+
+
     }
 }
