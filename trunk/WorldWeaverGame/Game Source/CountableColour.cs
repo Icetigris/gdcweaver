bd7c3dd18a80fc5d258753353371470711ce0753
Icetigris <Icetigris@9fadbaa6-2c71-11df-82fb-b9de231f855c>
2010-04-07 16:50:06 +0000

    Added CountableColour

---
+++ trunk/WorldWeaverGame/Game Source/CountableColour.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldWeaver
+{
+    public class CountableColour : IComparable
+    {
+        #region Class Variables
+        private int index;
+        private Particle.Colours colour;
+        #endregion
+
+        #region Properties
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        public Particle.Colours Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public CountableColour(int index, Particle.Colours colour)
+        {
+            this.index = index;
+            this.colour = colour;
+        }
+        #endregion
+
+        #region IComparable Members
+
+        public int CompareTo(object obj)
+        {
+            CountableColour inCC = (CountableColour)obj;
+            int returnValue = 0;
+
+            if (this.Index == inCC.Index)
+            {
+                returnValue = 0;
+            }
+            else if (this.Index < inCC.Index)
+            {
+                returnValue = -1;
+            }
+            else if (this.Index > inCC.Index)
+            {
+                returnValue = 1;
+            }
+
+            return returnValue;
+        }
+
+        #endregion
+    }
+}
