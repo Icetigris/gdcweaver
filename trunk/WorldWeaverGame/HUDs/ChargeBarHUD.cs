71d66cc0b6bf560981acfe0e68102bb5e00cc5da
Icetigris <Icetigris@9fadbaa6-2c71-11df-82fb-b9de231f855c>
2010-04-15 07:10:46 +0000

    Put actual value of charge on ChargeBarHUD.

--- trunk/WorldWeaverGame/HUDs/ChargeBarHUD.cs
+++ trunk/WorldWeaverGame/HUDs/ChargeBarHUD.cs
@@ -10,6 +10,9 @@ namespace WorldWeaver
     public class ChargeBarHUD : HUDElement
     {
         private Texture2D chargeBar;
+        private int charge;
+        private String chargeString = "";
+        SpriteFont spriteFont;
 
         public ChargeBarHUD()
         {
@@ -25,11 +28,23 @@ namespace WorldWeaver
             }
             */
             chargeBar = Globals.hudManager.Game.Content.Load<Texture2D>(TexturePath);
+            spriteFont = Content.Load<SpriteFont>("Fonts\\Arial");
 
             Position = new Rectangle((int)((Globals.hudManager.HudAreaWidth * 0.2f) - chargeBar.Width / 2),
                                           (int)((Globals.hudManager.HudAreaHeight - Globals.hudManager.HudAreaHeight * 0.1f) - chargeBar.Height / 2),
                                           chargeBar.Width,
                                           chargeBar.Height);
+            
+            charge = Globals.Player.calculateCurrentCharge();
+            if (charge > 0)
+            {
+                chargeString = "+";
+                chargeString += "" + charge;
+            }
+            else
+            {
+                chargeString = "" + charge;
+            }
         }
 
         public override void UnloadContent()
@@ -38,12 +53,23 @@ namespace WorldWeaver
 
         public override void Update()
         {
-            //nothing lol
+            chargeString = "";
+            charge = Globals.Player.calculateCurrentCharge();
+            if (charge > 0)
+            {
+                chargeString = "+" + charge;
+            }
+            else
+            {
+                chargeString = "" + charge;
+            }
         }
 
         public override void Draw(GameTime gameTime)
         {
             Globals.hudManager.SpriteBatch.Draw(chargeBar, Position, Color.White);
+            Globals.hudManager.SpriteBatch.DrawString(spriteFont, chargeString, (new Vector2(Position.Left + Position.Right/13, Position.Bottom - Position.Bottom / 13)), Color.White);
+
         }
     }
 }
